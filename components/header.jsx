417113c1fb0e6361a3784b9cbd94f53813e9f87d
Pranav Bhatia <pb200416@gmail.com>
2025-07-03 03:01:01 +0530

    c

--- components/header.jsx
+++ components/header.jsx
@@ -14,11 +14,11 @@ const Header = async () => {
       <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
         <Link href="/">
           <Image
-            src={"/logo1.png"}
+            src={"/logom.jpg"}
             alt="SpendSmart Logo"
             width={200}
             height={60}
-            className="h-12 w-auto object-contain"
+            className="h-24 w-auto object-contain -my-2"
           />
         </Link>
 
