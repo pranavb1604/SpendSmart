56d048ab9b9063b94dfc62ceb3b270d53c743adb
Pranav Bhatia <pb200416@gmail.com>
2025-07-03 12:48:14 +0530

    co

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ import { Toaster } from "sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Welth",
+  title: "SpendSmart",
   description: "One stop Finance Platform",
 };
 
